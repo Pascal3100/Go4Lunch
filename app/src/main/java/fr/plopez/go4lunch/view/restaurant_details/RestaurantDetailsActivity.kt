66d6e43ce19b7ee338e20fabd2516f44cf772b81
palopez <lopez.pascal.pro@gmail.com>
2021-07-23 18:09:24 +0200

    implementing restaurant details layout

---
+++ app/src/main/java/fr/plopez/go4lunch/view/restaurant_details/RestaurantDetailsActivity.kt
@@ -0,0 +1,12 @@
+package fr.plopez.go4lunch.view.restaurant_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.plopez.go4lunch.R
+
+class RestaurantDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant_details)
+    }
+}
