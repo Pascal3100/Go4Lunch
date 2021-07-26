519f2adb98c5b21540b0053f19d92ded374192e5
palopez <lopez.pascal.pro@gmail.com>
2021-07-26 16:30:48 +0200

    implementation of group collapse for login with email

--- app/src/main/java/fr/plopez/go4lunch/data/repositories/ActivePageRepository.kt
+++ app/src/main/java/fr/plopez/go4lunch/data/repositories/ActivePageRepository.kt
@@ -3,7 +3,7 @@ package fr.plopez.go4lunch.data.repositories
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
-class ActivePageRepository {
+class ActivePageRepository{
     private val _currentActivePageMutableLiveData = MutableLiveData(0)
     val currentActivePageLiveData:LiveData<Int> = _currentActivePageMutableLiveData
 
--- app/src/main/java/fr/plopez/go4lunch/view/landing_page/LandingPageActivity.kt
+++ app/src/main/java/fr/plopez/go4lunch/view/landing_page/LandingPageActivity.kt
@@ -1,9 +1,15 @@
 package fr.plopez.go4lunch.view.landing_page
 
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
+import androidx.transition.TransitionManager
 import fr.plopez.go4lunch.R
+import fr.plopez.go4lunch.databinding.ActivityLandingPageBinding
 
 class LandingPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +23,20 @@ class LandingPageActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         setContentView(R.layout.activity_landing_page)
+
+        val layout = findViewById<ConstraintLayout>(R.id.landing_page_container)
+        val button = findViewById<Button>(R.id.landing_page_email_login_button_selection)
+        val group = findViewById<androidx.constraintlayout.widget.Group>(R.id.landing_page_email_login_group)
+
+        // Manage loggin with email display
+        button.setOnClickListener {
+
+            Log.d("TAG", "##### onCreate() called")
+            // Transition to show login with email stuff when button clicked
+            TransitionManager.beginDelayedTransition(layout)
+            // Clears the selection button
+            it.isVisible = false
+            group.isVisible = true
+        }
     }
 }
