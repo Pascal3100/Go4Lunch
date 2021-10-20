8a41fe6324cf263e8adca874d42f878d0ea646b6
palopez <lopez.pascal.pro@gmail.com>
2021-10-20 16:15:17 +0200

    custom snack bar rebuilt but not ok

--- app/src/main/java/fr/plopez/go4lunch/utils/CustomSnackBar.kt
+++ app/src/main/java/fr/plopez/go4lunch/utils/CustomSnackBar.kt
@@ -2,10 +2,10 @@ package fr.plopez.go4lunch.utils
 
 import android.content.Context
 import android.view.View
+import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import fr.plopez.go4lunch.R
-import fr.plopez.go4lunch.view.main_activity.MainActivity
 
 class CustomSnackBar constructor(private val parentView: View,
                                  private val parentContext: Context) {
@@ -47,4 +47,55 @@ class CustomSnackBar constructor(private val parentView: View,
         snackbar.show()
     }
 
+}
+
+
+class CustomSnackBar2 {
+    data class Builder(
+        var message: String? = null,
+        @get:ColorInt
+        var backgroundTintColorInt: Int = R.color.grey,
+        @get:ColorInt
+        var textColorInt: Int = R.color.white,
+
+        var parentView: View,
+        var parentContext: Context
+    ) {
+        fun setMessage(message: String) = apply {
+            this.message = message
+        }
+
+        fun setMessageType(type: String) = apply {
+            when (type) {
+                "warning" -> {
+                    backgroundTintColorInt = R.color.yellow_warning
+                    textColorInt = R.color.white
+                }
+                "error" -> {
+                    backgroundTintColorInt = R.color.redish_whisky
+                    textColorInt = R.color.white
+                }
+                else -> {
+                    backgroundTintColorInt = R.color.grey
+                    textColorInt = R.color.white
+                }
+            }
+        }
+
+        fun setParentView(view: View) = apply {
+            parentView = view
+        }
+
+        fun setParentContext(context: Context) = apply {
+            parentContext = context
+        }
+
+        fun build() = Snackbar.make(
+            parentView,
+            requireNotNull(message, { "require not null message" }),
+            Snackbar.LENGTH_SHORT
+        ).setBackgroundTint(ContextCompat.getColor(parentContext, backgroundTintColorInt))
+            .setTextColor(ContextCompat.getColor(parentContext, textColorInt))
+            .show()
+    }
 }
