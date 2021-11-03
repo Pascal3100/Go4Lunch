91b2ce71ca7e96da8387034722a449391fe6413a
Nino <anthony@delcey.fr>
2021-11-03 17:04:12 +0100

    Nino to the rescuuuue

--- app/src/main/java/fr/plopez/go4lunch/view/restaurant_details/RestaurantDetailsActivity.kt
+++ app/src/main/java/fr/plopez/go4lunch/view/restaurant_details/RestaurantDetailsActivity.kt
@@ -2,10 +2,10 @@ package fr.plopez.go4lunch.view.restaurant_details
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
@@ -42,8 +42,7 @@ class RestaurantDetailsActivity : AppCompatActivity() {
 
         restaurantDetailsViewModel.onPlaceIdRequest(placeId)
 
-        restaurantDetailsViewModel.restaurantDetailsViewLiveData.observe(this@RestaurantDetailsActivity) {
-            restaurantDetailsViewState ->
+        restaurantDetailsViewModel.restaurantDetailsViewLiveData.observe(this) { restaurantDetailsViewState ->
             // Glide section
             Glide.with(binding.root)
                 .load(restaurantDetailsViewState.photoUrl)
@@ -58,7 +57,7 @@ class RestaurantDetailsActivity : AppCompatActivity() {
             binding.restaurantDetailsActivitySubtitle.text = restaurantDetailsViewState.address
 
             // listener for call button
-            binding.phoneButton?.setOnClickListener {
+            binding.phoneButton.setOnClickListener {
                 Log.d("TAG", "phoneButton")
                 val dialIntent = Intent(Intent.ACTION_DIAL)
                 dialIntent.data = Uri.parse("tel:" + restaurantDetailsViewState.phoneNumber)
@@ -66,7 +65,7 @@ class RestaurantDetailsActivity : AppCompatActivity() {
             }
 
             // listener for website button
-            binding.websiteButton?.setOnClickListener {
+            binding.websiteButton.setOnClickListener {
                 Log.d("TAG", "websiteButton")
                 val websiteIntent = Intent(Intent.ACTION_VIEW)
                 websiteIntent.data = Uri.parse(restaurantDetailsViewState.website)
@@ -75,18 +74,19 @@ class RestaurantDetailsActivity : AppCompatActivity() {
         }
 
         // listener for like button state
+        // TODO Pascal : à merger avec le viewstate (une seule LiveData d'état), et puis pas de if ! :p
         restaurantDetailsViewModel.likeStateLiveData.observe(this@RestaurantDetailsActivity) {
             if (it) {
-                binding.ratingButton?.setCompoundDrawables(
+                binding.ratingButton.setCompoundDrawables(
                     null,
                     getDrawable(R.drawable.ic_baseline_star_rate_24),
                     null,
                     null
                 )
             } else {
-                binding.ratingButton?.setCompoundDrawables(
+                binding.ratingButton.setCompoundDrawables(
                     null,
-                    getDrawable(R.drawable.ic_baseline_empty_star_24),
+                    getDrawable(R.drawable.ic_baseline_star_rate_24),
                     null,
                     null
                 )
@@ -94,12 +94,10 @@ class RestaurantDetailsActivity : AppCompatActivity() {
         }
 
         // listener for like button
-        binding.ratingButton?.setOnClickListener {
+        binding.ratingButton.setOnClickListener {
             Log.d("TAG", "ratingButton")
             restaurantDetailsViewModel.onLike()
         }
-
-
 
         // listener for back navigation
         binding.restaurantDetailsActivityToolbar.setNavigationOnClickListener {
